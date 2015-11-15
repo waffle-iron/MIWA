3f089ae2bf8e28951a68d3f712c327cc9c65e986
Gauthier FRIDIERE <gauthier.fridiere@gmail.com>
2015-11-15 18:17:38 +0100

    Adding Util class

---
+++ TechnicalBase/MainWebService/src/main/java/com/miwa/util/PojoUtil.java
@@ -0,0 +1,21 @@
+package com.miwa.util;
+
+import com.miwa.model.Callback;
+import com.miwa.ws.pojo.CallbackPOJO;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class PojoUtil {
+
+    public static LinkedList<CallbackPOJO> toPojo(List<Callback> list) {
+        LinkedList<CallbackPOJO> retList = new LinkedList<CallbackPOJO>();
+        for (Callback c : list) {
+            CallbackPOJO callbackPOJO = new CallbackPOJO(c.getCron(),c.getMessage(), c.getEndpoint());
+            callbackPOJO.setService_name(c.getService().getName());
+            retList.add(callbackPOJO);
+        }
+
+        return retList;
+    }
+}
