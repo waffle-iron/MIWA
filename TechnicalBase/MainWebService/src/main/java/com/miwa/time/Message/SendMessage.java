388417b70c144a926cf04d1b4a3d5aa6ab19de36
julienzup <julien.bohon@gmail.com>
2016-01-16 12:39:22 +0100

    add try catch in send message

--- TechnicalBase/MainWebService/src/main/java/com/miwa/time/Message/SendMessage.java
+++ TechnicalBase/MainWebService/src/main/java/com/miwa/time/Message/SendMessage.java
@@ -14,47 +14,47 @@ public class SendMessage{
 
     public void send() {
 //        System.out.println("now : " + new Date() + "  |  virtual date : " + TimeManager.GetInstance().getCurrentDate());
-        Client client = Client.create();
+        try {
+            Client client = Client.create();
 
-        WebResource webResource = client
-                .resource(callback.getService().getHostname()+ ":" +String.valueOf(callback.getService().getPort()) + callback.getEndpoint());
+            WebResource webResource = client
+                    .resource(callback.getService().getHostname() + ":" + String.valueOf(callback.getService().getPort()) + callback.getEndpoint());
 
-        ClientResponse response;
-        if ("POST".equals(callback.getRequestType()))
-        {
-            System.out.println("send post request");
-            response = webResource.post(ClientResponse.class, callback.getMessage());
+            ClientResponse response;
+            if ("POST".equals(callback.getRequestType())) {
+                System.out.println("send post request");
+                response = webResource.post(ClientResponse.class, callback.getMessage());
 
-            if (response.getStatus() != 201) {
-                throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
+                if (response.getStatus() != 201) {
+                    throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
+                }
+            } else if ("PUT".equals(callback.getRequestType())) {
+                System.out.println("send put request");
+                response = webResource.put(ClientResponse.class, callback.getMessage());
+
+                if (response.getStatus() != 200) {
+                    throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
+                }
+            } else if ("DELETE".equals(callback.getRequestType())) {
+                System.out.println("send delete request");
+                response = webResource.delete(ClientResponse.class, callback.getMessage());
+
+                if (response.getStatus() != 200) {
+                    throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
+                }
+            } else {
+                System.out.println("send get request");
+                response = webResource.get(ClientResponse.class);
+                if (response.getStatus() != 200) {
+                    throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
+                }
             }
         }
-        else if ("PUT".equals(callback.getRequestType()))
-        {
-            System.out.println("send put request");
-            response = webResource.put(ClientResponse.class, callback.getMessage());
+        catch (Exception ex){
+            System.err.println(ex.getMessage() + "   |   " + callback.getService() + "   |   " + callback.getEndpoint()
+                    + "   |   " + callback.getRequestType() + "   |   " + callback.getMessage());
+        }
 
-            if (response.getStatus() != 200) {
-                throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
-            }
-        }
-        else if ("DELETE".equals(callback.getRequestType()))
-        {
-            System.out.println("send delete request");
-            response = webResource.delete(ClientResponse.class, callback.getMessage());
-
-            if (response.getStatus() != 200) {
-                throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
-            }
-        }
-        else
-        {
-            System.out.println("send get request");
-            response = webResource.get(ClientResponse.class);
-            if (response.getStatus() != 200) {
-                throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
-            }
-        }
     }
 }
 
